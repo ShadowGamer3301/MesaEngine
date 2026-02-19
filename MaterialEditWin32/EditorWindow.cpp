08d18c3f6adacf90a4f1918b1f7d7869f48d3634
ShadowGamer3301 <ShadowGamer3301@protonmail.com>
2026-02-19 14:49:40 +0100

    Add comments to editor window

--- MaterialEditWin32/EditorWindow.cpp
+++ MaterialEditWin32/EditorWindow.cpp
@@ -1,17 +1,27 @@
 #include "EditorWindow.h"
 #include "EditorCallbacks.h"
 
+/*
+	Constructor. Initializes main editor window and its widgets
+*/
 EditorWindow::EditorWindow(uint32_t width, uint32_t height, const char* name)
 {
+	// Create main window
 	p_Window = new Fl_Window(width, height, name);
+	// Validate creation results
 	if (!p_Window) throw Mesa::Exception();
+	// Add close callback
 	p_Window->callback(WindowCloseCallback);
 
+	// Create preview window
 	p_ViewWindow = new Fl_Window(10, 10, 800, 600, nullptr);
+	// Validate creation results
 	if (!p_ViewWindow) throw Mesa::Exception();
+	// Embbed preview window in main window
 	p_ViewWindow->show();
 	p_ViewWindow->end();
 
+	// Create input widgets and add their callbacks
 	p_ModelInput = new Fl_Input(970, 10, 200, 40, "Model: ");
 	p_ModelInput->callback(ModelTextCallback);
 
@@ -21,6 +31,7 @@ EditorWindow::EditorWindow(uint32_t width, uint32_t height, const char* name)
 	p_SpecularShaderInput = new Fl_Input(970, 90, 200, 40, "Color pass shader: ");
 	p_SpecularShaderInput->callback(SpecularTextCallback);
 
+	// Create load buttons and add their callbacks
 	p_ModelLoad = new Fl_Button(1180, 10, 50, 40, "Load");
 	p_ModelLoad->callback(ModelLoadCallback);
 
@@ -30,12 +41,17 @@ EditorWindow::EditorWindow(uint32_t width, uint32_t height, const char* name)
 	p_SpecularLoad = new Fl_Button(1180, 90, 50, 40, "Load");
 	p_SpecularLoad->callback(SpecularLoadCallback);
 
+	// Show main window
 	p_Window->end();
 	p_Window->show();
 }
 
+/*
+	Destructor. Ensures that all window components are freed properly.
+*/
 EditorWindow::~EditorWindow()
 {
+	// Free all windows widgets
 	if (p_ModelLoad) delete p_ModelLoad;
 	if (p_ColorLoad) delete p_ColorLoad;
 	if (p_SpecularLoad) delete p_SpecularLoad;
@@ -46,9 +62,13 @@ EditorWindow::~EditorWindow()
 
 	if (p_ViewWindow) delete p_ViewWindow;
 
+	// Free main window
 	if (p_Window) delete p_Window;
 }
 
+/*
+	Returns native WIN32 window handle for preview window.
+*/
 HWND EditorWindow::GetNativeViewerHandle()
 {
 	return fl_xid(p_ViewWindow);
--- MaterialEditWin32/EditorWindow.h
+++ MaterialEditWin32/EditorWindow.h
@@ -13,12 +13,14 @@ private:
 	Fl_Window* p_Window = nullptr; // Main application window
 	Fl_Window* p_ViewWindow = nullptr; // Material preview window
 
+	// Input widgets
 	Fl_Input* p_ModelInput = nullptr;
 	Fl_Input* p_ColorShaderInput = nullptr;
 	Fl_Input* p_SpecularShaderInput = nullptr;
-	Fl_Input* p_NormalShaderInput = nullptr;
-	Fl_Input* p_PositionShaderInput = nullptr;
+	Fl_Input* p_NormalShaderInput = nullptr; // Unused
+	Fl_Input* p_PositionShaderInput = nullptr; // Unused
 
+	// Button widgets
 	Fl_Button* p_ModelLoad = nullptr;
 	Fl_Button* p_ColorLoad = nullptr;
 	Fl_Button* p_SpecularLoad = nullptr;
